2eac862a7b3222557f299fc8991f53227805808b
Roma93 <romaricsedogo95@gmail.com>
2024-05-18 18:47:36 +0200

    faceSnap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { FaceSnap } from './models/face-snaps.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FaceSnapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'snapface';
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+
+  ngOnInit() {
+    this.mySnap = new FaceSnap (
+      'Archibald',
+      'Mon meilleur ami depuis tout petit',
+      'assets/img/goku.png',
+       new Date(),
+       0,
+    )
+  }
 }
